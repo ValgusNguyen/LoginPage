efa68c5fd7e04c441b9821dc9e589a538ce68a15
Valgus <khuenguyenhaminh.com>
2024-06-06 18:00:26 +0700

    feat: add env into next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_URL : 'https://zxmckhufnkxrjovnfupz.supabase.co',
+        NEXT_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4bWNraHVmbmt4cmpvdm5mdXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NDE4MTEsImV4cCI6MjAyOTUxNzgxMX0.jRxfYOfswArXytnl_1GM4DfLVZzYCCPM0ehsbqL2NFs'
+      },
+};
 
 export default nextConfig;
