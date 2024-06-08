b6c43bc4c4df88cc07aa6377ec79b88dac40fe0a
Valgus <khuenguyenhaminh.com>
2024-06-08 20:10:53 +0700

    change supabase key

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_URL : 'https://zxmckhufnkxrjovnfupz.supabase.co',
-        NEXT_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4bWNraHVmbmt4cmpvdm5mdXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NDE4MTEsImV4cCI6MjAyOTUxNzgxMX0.jRxfYOfswArXytnl_1GM4DfLVZzYCCPM0ehsbqL2NFs'
+        NEXT_URL : 'http://192.168.3.107:54321',
+        NEXT_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
       },
 };
 
