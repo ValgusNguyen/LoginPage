dc4fc132dbb66ca63e0214728587aa8da3874120
Valgus <khuenguyenhaminh.com>
2024-06-08 18:23:56 +0700

    feat: add supabase auth

--- src/app/component/register.tsx
+++ src/app/component/register.tsx
@@ -1,50 +1,50 @@
+"use client"
 import React, { useState } from "react";
 import "./login.css";
-import {useForm} from 'react-hook-form'
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from 'next/navigation'
+import { useForm } from "react-hook-form";
+import { redirect } from "next/navigation";
+import supabase from "../../../config";
+import { createClient } from "@supabase/supabase-js";
 
-const Register = async () => {
-  const supabase = createClient();
-  const signUp = async () => {               
-    await supabase.auth.signUp({
-      email,
-      password,
-      options: {},
+
+const Register =  () => {
+  const {register, handleSubmit, formState: {errors} } = useForm()
+  const onSubmit = (data: any) => registerUser(data)
+  type registerUser = {
+    username: string;
+    email: string;
+    password: string;
+  }
+  const registerUser  = (data: registerUser) => { 
+    supabase.auth.signUp({
+      email: data.email ,
+      password: data.password,
+      options: {
+        data: {
+          username: data.username,
+        }
+      }
     });
   };
-  const { error } = await supabase.auth.signUp(data);
-  if (error) {
-    redirect(`${location.origin}/blank`);
+  if (errors) {
+    console.log(errors.error_description || errors.message)
+  } else {
+    console.log('Check your email for the link!')
   }
+
   return (
     <div className="w-[60%] h-4/5 rounded-lg p-6">
-      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-      <form>
-        <h1 className="text-4xl ">Create new account</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="text-4xl text-white ">Create new account</h1>
         <div className="register-link">
-          <p>
+          <p className="text-[#F6F4F4">
             Already A Member? <a href="#">Log in</a>
           </p>
         </div>
-        {/* <div className="flex relative w-full h-16 mt-7">
-          <h2 className="text-white">First Name</h2>
-          <input
-            type="text"
-            className="w-full h-[60%] bg-transparent border-2 rounded border-red-50 text-xl p-3"
-            required
-          />
-          <h2 className="text-white">Last Name</h2>
-          <input
-            type="text"
-            className="w-full h-[60%] bg-transparent border-2 rounded border-red-50 text-xl p-3"
-            required
-          /> */}
-        {/* </div> */}
         <div className="relative w-full h-16 mt-7">
           <h2>Username</h2>
           <input
-            type="text"
+            type="text" {...register("username", { required: true })}
             className="w-full h-[60%] bg-transparent border-2 rounded border-red-50 text-xl p-3"
             required
           />
@@ -52,10 +52,11 @@ const Register = async () => {
         <div className="relative w-full h-16 mt-7">
           <h2>Email</h2>
           <input
-            type="email"
-            // {...register("email")}
-            className="w-full h-[60%] bg-transparent border-2 rounded border-red-50 text-xl p-3"
-            placeholder="Email"
+            type="email" {...register("email", { required: true })}
+            name="email" 
+            // value={email}
+            className="w-full h-[60%] bg-transparent border-2 rounded border-red-50 text-xl p-3" 
+            placeholder="Email" 
             required
           />
         </div>
@@ -63,7 +64,7 @@ const Register = async () => {
           <h2>Password</h2>
           <input
             type="password"
-            // {...register("password")}
+            {...register("password", { required: true })}
             className="w-full h-[60%] bg-transparent border-2 rounded border-red-50 text-xl p-3"
             placeholder="Password"
             required
@@ -74,10 +75,11 @@ const Register = async () => {
           <button
             className="w-[60%] h-10 border-2 rounded border-red-50"
             type="submit"
-            onClick={signUp}
+            // onClick={handleSubmit((data) => setData(JSON.stringify(data))}
           >
             Create Account
           </button>
+          {/* {formError && <p>{formError}</p>} */}
         </div>
       </form>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,13 @@
 import Image from "next/image";
 import  Login  from "./component/login";
 import  Register  from "./component/register";
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as z from 'zod';
-import { createClient } from '@/utils/supabase/server';
+// import { zodResolver } from '@hookform/resolvers/zod';
+// import * as z from 'zod';
+import supabase from "../../config";
 
-// export default async function Notes() {
-//   const supabase = createClient();
-//   const { data: notes } = await supabase.from("notes").select();
-//   return <pre>{JSON.stringify(notes, null, 2)}</pre>
-// }
+
 export default function Home() {
+  // console.log(supabase)
   return (
     <Register />
   //  <Login />
